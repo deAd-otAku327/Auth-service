3a088ba615462d3a871f670d2e32dc7647d62136
deAd-otAku327 <almchech@mail.ru>
2025-07-07 03:43:43 +0300

    improved tokenizer for refresh tokens gen

--- internal/tokenizer/tokenizer.go
+++ internal/tokenizer/tokenizer.go
@@ -1,59 +1,66 @@
 package tokenizer
 
 import (
+	"encoding/base64"
 	"errors"
+	"net/http"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
+
+const RefreshCookieName = "refresh_session"
 
 var (
-	ErrTokenGeneration = errors.New("access token generation failed")
-	ErrTokenInvalid    = errors.New("access token invalid")
-	ErrTokenExpired    = errors.New("access token expired")
+	ErrTokenInvalid = errors.New("access token invalid")
+	ErrTokenExpired = errors.New("access token expired")
 )
 
-type JWTTokenizer interface {
-	GenerateAccessToken(userID string) (*string, error)
-	VerifyAccessToken(tokenString string) (jwt.MapClaims, error)
+type Tokenizer interface {
+	GenerateAccessTokenJWT(userID string) (*string, error)
+	VerifyAccessTokenJWT(tokenString string) (jwt.MapClaims, error)
+	GenerateRefreshTokenCookie() *http.Cookie
 }
 
 type tokenizer struct {
-	tokenIssuer string
-	secretKey   []byte
-	tokenExpire time.Duration
+	tokenIssuer        string
+	jwtSecret          []byte
+	accessTokenExpire  time.Duration
+	refreshTokenExpire time.Duration
 }
 
-func New(iss, key string, expire time.Duration) JWTTokenizer {
+func New(iss, jwtKey string, accessExpire, refreshExpire time.Duration) Tokenizer {
 	return &tokenizer{
-		tokenIssuer: iss,
-		secretKey:   []byte(key),
-		tokenExpire: expire,
+		tokenIssuer:        iss,
+		jwtSecret:          []byte(jwtKey),
+		accessTokenExpire:  accessExpire,
+		refreshTokenExpire: refreshExpire,
 	}
 }
 
-func (t *tokenizer) GenerateAccessToken(userGUID string) (*string, error) {
+func (t *tokenizer) GenerateAccessTokenJWT(userGUID string) (*string, error) {
 	claims := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
 		"sub": userGUID,
 		"iss": t.tokenIssuer,
-		"exp": time.Now().Add(t.tokenExpire).Unix(),
+		"exp": time.Now().Add(t.accessTokenExpire).Unix(),
 		"iat": time.Now().Unix(),
 	})
 
-	accessToken, err := claims.SignedString(t.secretKey)
+	accessToken, err := claims.SignedString(t.jwtSecret)
 	if err != nil {
-		return nil, ErrTokenGeneration
+		return nil, err
 	}
 
 	return &accessToken, nil
 }
 
-func (t *tokenizer) VerifyAccessToken(tokenString string) (jwt.MapClaims, error) {
+func (t *tokenizer) VerifyAccessTokenJWT(tokenString string) (jwt.MapClaims, error) {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if token.Method.Alg() != jwt.SigningMethodHS512.Alg() {
 			return nil, ErrTokenInvalid
 		}
-		return t.secretKey, nil
+		return t.jwtSecret, nil
 	})
 	if err != nil {
 		if errors.Is(err, jwt.ErrTokenExpired) {
@@ -67,4 +74,14 @@ func (t *tokenizer) VerifyAccessToken(tokenString string) (jwt.MapClaims, error)
 	}
 
 	return nil, ErrTokenInvalid
+}
+
+func (t *tokenizer) GenerateRefreshTokenCookie() *http.Cookie {
+	refreshToken := base64.StdEncoding.EncodeToString([]byte(uuid.NewString()))
+	return &http.Cookie{
+		Name:    RefreshCookieName,
+		Value:   refreshToken,
+		Expires: time.Now().Add(t.refreshTokenExpire),
+		Path:    "/api/auth/refresh",
+	}
 }
